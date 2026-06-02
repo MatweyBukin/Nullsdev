6aee5d3d95e6383b3a7bc6ea0cbf49dd26e9eda5
Матвей <bukin.matv@gmail.com>
2026-06-02 22:08:29 +0300

    ddd

--- nullsdev/src/App.js
+++ nullsdev/src/App.js
@@ -2,14 +2,14 @@ import './css/App.css';
 import Header from './components/Header';
 import Menu from './components/Menu';
 import {createContext, useState} from "react"
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {HashRouter, Routes, Route} from "react-router-dom"
 import Portfolio from './components/Portfolio';
 
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Header/>
         <main>
           <Routes>
@@ -17,7 +17,7 @@ function App() {
             <Route path='/portfolio' element={<Portfolio/>}/>
           </Routes>
         </main>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
