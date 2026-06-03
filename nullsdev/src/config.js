b5094b6106e6656bdf3f1e3da52f3eda996ff09e
Матвей <bukin.matv@gmail.com>
2026-06-03 19:03:25 +0300

    Добавил иконку, изменил manifest, добавил в протфолио ..

--- nullsdev/src/config.js
+++ nullsdev/src/config.js
@@ -1,5 +1,8 @@
 export const telegramLink = "https://t.me/m1twey"
 
 export const portfolio = [
-    {img: "this_website.png", description: "Этот сайт)"},
+    {img: "this_website.png", description: "Этот вебсайт)"},
+    {img: "telegram.png", description: "Телеграм-бот для записи на прием"},
+    {img: "telegram.png", description: "Телеграм-юзербот для продажи цифровых товаров с ии"},
+    {img: "pickleball.png", description: "Вебсайт для записи на пиклбол"},
 ]
